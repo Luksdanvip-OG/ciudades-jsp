3d1bdb24bc8ee9b586278451383ba1d11ec1055d
lucas <lucasbarberisperez@gmail.com>
2023-03-12 19:11:34 +0100

    Modificado conexion a mysql

--- src/main/java/configuracion/PropiedadesConexion.java
+++ src/main/java/configuracion/PropiedadesConexion.java
@@ -1,7 +1,7 @@
 package configuracion;
 
 public class PropiedadesConexion {
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/world?useSSL=false&allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    public static final String JDBC_URL = "jdbc:mysql://20.199.44.70:3306/world?useSSL=false&allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     public static final String JDBC_USER = "gestionciudades";
     public static final String JDBC_PASSWORD = "gestionciudades";
 }
